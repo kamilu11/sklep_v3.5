d7ca7c32dfb3840cd8129766659c49e4138c5c02
kamilu11 <76968105+kamilu11@users.noreply.github.com>
2026-04-22 08:54:17 +0200

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -1,3 +1,3 @@
 window.APP_CONFIG = {
-  backendUrl: "https://script.google.com/macros/s/AKfycbwuZ7kjaheSF1RlFB_TeA07YBCzKjsAhstedRsLXhsG7ENQv1XZkeG88GSnM0murho0/exec"
+  backendUrl: "https://script.google.com/macros/s/AKfycbyitnrLa2xbO5R96Y2u9nZRo3KHsfURPqW1rXS4W55U8EaG92g7n1UTp6PQGBq8FTWu/exec"
 };
